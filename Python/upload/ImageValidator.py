84d7a94c687499abdee18a013ddef9b8597bb4d8
website-developer7 <mkids1140@gmail.com>
2026-04-12 13:11:53 +0200

    feat: add ImageValidator class with methods for image validation processes

---
+++ Python/upload/ImageValidator.py
@@ -0,0 +1,40 @@
+
+
+
+
+class ImageValidator:
+    """ 
+    step 1: Image Vali
+    """
+    def __init__(self):
+        pass
+
+    def load_image():
+        pass
+
+    def validate_format():
+        pass
+
+    def validate_size():
+        pass
+
+    def detect_faces():
+        pass
+
+    def validate_single_face():
+        pass
+
+    def validate_face_quality():
+        pass
+
+    def validate_background():
+        pass
+
+    def validate_blur():
+        pass
+
+    def validate_brightness():
+        pass
+
+    def final_validator():
+        pass
